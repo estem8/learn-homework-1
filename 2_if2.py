65ea7ccd1906c3cc10d14f262223918ba5f04905
estem8 <awards@mail.ru>
2023-11-19 14:20:12 +0300

    2ое задание

--- 2_if2.py
+++ 2_if2.py
@@ -14,13 +14,23 @@
   и выводя на экран результаты
 
 """
+def checker(input_a, input_b):
+    if not isinstance(input_a, str) or not isinstance(input_b, str):
+      return 0    
+    if input_a == input_b:
+      return 1
+    if input_a != input_b and len(input_a) > len(input_b):
+      return 2
+    if input_a != input_b and input_b == 'learn':
+      return 3
+
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    print(checker(1,'text'))
+    print(checker('text','text'))
+    print(checker('text-1','text'))
+    print(checker('text','learn'))
+    
     
 if __name__ == "__main__":
     main()
