a268615099d662769e77075a749f3c59198b5fe0
estem8 <awards@mail.ru>
2023-11-19 15:04:39 +0300

    4 задание

--- 4_while1.py
+++ 4_while1.py
@@ -11,10 +11,9 @@
 
 
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
+    while True:
+      if input(f'Как дела?\n').capitalize() == 'Хорошо':
+        break
 
     
 if __name__ == "__main__":
