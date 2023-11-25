30e8e923c0067303d29136e9467e03bab0560ffd
estem8 <awards@mail.ru>
2023-11-25 11:45:24 +0300

    Fix

--- 10_bot.py
+++ 10_bot.py
@@ -1,16 +1,24 @@
 
 from settings import BOT_TOKEN
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
 import ephem
 from datetime import date
 import logging
+from collections import defaultdict
 
 logging.basicConfig(level=logging.INFO)
-   
+
+user_word_frequencies = {}
+word_frequency = defaultdict(int)
+def process_message(incoming_message):
+    words = incoming_message.split()
+    for word in words:
+        normalized_word = word.lower()
+        word_frequency[normalized_word] += 1
+        return word_frequency
 
 def checker(incoming_message: str) -> str:    
-    logging.info(incoming_message.split())
     for item in incoming_message.split():
         if hasattr(ephem, item):
             logging.info(f'Попался {item}')
@@ -18,13 +26,13 @@ def checker(incoming_message: str) -> str:
             return cont_result
     return f'Введены ошибочные данные'
 
-async def echo(update: Update, context):
+async def echo(update: Update, some_context):
+    await update.message.reply_text(process_message(update.message.text))
     await update.message.reply_text(checker(update.message.text))
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT, echo))
     app.run_polling(allowed_updates=Update.ALL_TYPES)
-
 if __name__ == '__main__':
     main()
